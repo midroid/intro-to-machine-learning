1087681b089ca67b2b3b14147d2853baec8fbc68
Chauhan Akash(チョウハン　アカーシュ) <ch.m.aakash@gmail.com>
2017-01-20 09:34:52 +0000

    exercise naive base completed

---
+++ Naive Bayes/base.py
@@ -0,0 +1,19 @@
+# Naive Base
+from sklearn import datasets
+iris = datasets.load_iris()
+from sklearn.naive_bayes import GaussianNB
+gnb = GaussianNB()
+y_pred = gnb.fit(iris.data, iris.target).predict(iris.data)
+print("mislead %d: %d"
+    % (iris.data.shape[0], (iris.target != y_pred).sum()))
+    
+    
+# SVM - Support Vector Machine
+from sklearn import svm
+X = [[0,0],[1,1]]
+y = [0,1]
+clf = svm.SVC()
+clf.fit(X, y)
+print(clf.predict([[2., 2.]]))
+
+#kernel trick - multidimensinoal to make it linear separable
