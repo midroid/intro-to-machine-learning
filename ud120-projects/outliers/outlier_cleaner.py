8e2c8bd1b04682fdc740cb5f915335736ec0a6ed
midroid <ch.m.aakash@gmail.com>
2017-02-09 05:55:46 +0900

    outlier cleaner function implemented

--- ud120-projects/outliers/outlier_cleaner.py
+++ ud120-projects/outliers/outlier_cleaner.py
@@ -7,14 +7,31 @@ def outlierCleaner(predictions, ages, net_worths):
         residual errors (difference between the prediction
         and the actual net worth).
 
-        Return a list of tuples named cleaned_data where 
+        Return a list of tuples named cleaned_data where
         each tuple is of the form (age, net_worth, error).
     """
-    
+
     cleaned_data = []
 
     ### your code goes here
+    predError = []
+    errd = 0
+    erra = []
+    indexa = []
+    ind = 0
 
-    
+    for i in range(len(ages)):
+        erra.append(abs( predictions[i][0] - net_worths[i][0] ))
+    for j in range(9):
+        ind = erra.index(max(erra))
+        indexa.append(erra.index(max(erra)))
+        erra[ind] = 0
+
+    for i in range(len(ages)):
+        if ( i in indexa ):
+            pass
+        else:
+            errd = predictions[i][0] - net_worths[i][0]
+            cleaned_data.append([ages[i][0], net_worths[i][0], errd])
+
     return cleaned_data
-
